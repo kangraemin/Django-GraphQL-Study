cb024b0bf09cebf28e3b3a3d7cea7e851aa06ab0
kangraemin <remin1994@gmail.com>
2020-10-01 19:03:20 +0900

    Add room schema & resolve using graphene-django - Add roomType ( only graphene, graphene-django )

--- config/schema.py
+++ config/schema.py
@@ -1,8 +1,24 @@
 import graphene
+from rooms.models import Room
 
+from graphene_django import DjangoObjectType
+
+
+class RoomType(DjangoObjectType):
+    class Meta:
+        model = Room
+
+# class RoomType(graphene.ObjectType):
+#     name = graphene.String()
+#     address = graphene.String()
+#     price = graphene.Int()
+#     beds = graphene.Int()
 
 class Query(graphene.ObjectType):
     hello = graphene.String()
+    # Field => Use for complex type (not String, boolean ... etc)
+    # rooms = graphene.Field(type=RoomType)
+    rooms = graphene.List(RoomType)
 
     def resolve_hello(self, info):
         # Information object all of resolver 
@@ -10,6 +26,9 @@ class Query(graphene.ObjectType):
         # print(info.context)
         # print(info.context.user)
         return "Hello"
+
+    def resolve_rooms(self, info):
+        return Room.objects.all()
 
 class Mutation():
     pass
